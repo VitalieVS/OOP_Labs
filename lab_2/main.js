65c90664c2e6e97b5c77e92150664f09bc4d5261
VitalieVS <54939321+VitalieVS@users.noreply.github.com>
2020-01-28 22:03:24 +0200

    Add files via upload

--- lab_2/main.js
+++ lab_2/main.js
@@ -22,14 +22,7 @@ class Perfect {
 document.getElementById("check_number_but").addEventListener("click", () => {
     let number = document.getElementById("number").value;
     const result = new Perfect(number);
-
-    if (result.isPerfect()) {
-        document.getElementById('number_is_perfect_result').style.display = 'block';
-        document.getElementById('number_not_perfect_result').style.display = 'none';
-    } else {
-        document.getElementById('number_not_perfect_result').style.display = 'block';
-        document.getElementById('number_is_perfect_result').style.display = 'none';
-    }
+    result.isPerfect() ? document.getElementById('number_is_perfect_result').style.display = 'block' : document.getElementById('number_not_perfect_result').style.display = 'block';
 });
 
 document.getElementById("interval_but").addEventListener("click", () => {
@@ -39,40 +32,48 @@ document.getElementById("interval_but").addEventListener("click", () => {
     const notPerfect = document.getElementById("not_perfect_from_to");
     perfect.style.display = "block";
     notPerfect.style.display = "block";
+    let perfectNumbers = [];
+    let notPerfectNumbers = [];
 
     for (let i = value_from; i <= value_to; i++) {
         let result = new Perfect(i);
-        if (result.isPerfect()) {
-            perfect.innerHTML += i + ',';
-        } else {
-            notPerfect.innerHTML += i + ',';
-        }
+        result.isPerfect() ? perfectNumbers.push(i) : notPerfectNumbers.push(i);
     }
+
+    perfect.innerHTML = "Perfect Numbers" + perfectNumbers;
+    notPerfect.innerHTML = "Not perfect Numbers" + notPerfectNumbers;
 });
 
 document.getElementById("first_numbers_but").addEventListener("click", () => {
-    const count = document.getElementById("first_numbers_count").value;
+    const count = Number(document.getElementById("first_numbers_count").value);
     const first_numbers = document.getElementById("first_numbers_result");
     let validCount = 0;
+    let array = [];
+    let i = 0;
 
-    for (let i = 0; i < Infinity; i++) {
+    while (validCount !== count) {
         let result = new Perfect(i);
         if (result.isPerfect()) {
-            first_numbers.innerHTML += i + ',';
+            array.push(i);
             validCount++;
         }
-        if (validCount == count) break;
+        i++
     }
+    first_numbers.innerHTML = array;
 });
 
 document.getElementById("max_perfect_number_but").addEventListener("click", () => {
     const writeResult = document.getElementById("max_perfect_number_result");
+    let validCount = 0;
+    let count = 1;
+    let i = Infinity;
 
-    for (let i = Number.MAX_VALUE; i > 1; i--) {
+    while (validCount !== Number(count)) {
         let result = new Perfect(i);
         if (result.isPerfect()) {
-            writeResult.innerHTML = "Highest perfect number is:" + i;
-            break;
+            writeResult.innerHTML = "Max number:" + i;
+            validCount++;
         }
+        i--;
     }
 });
