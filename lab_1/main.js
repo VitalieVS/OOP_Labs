5bd1da373eed24897b43409ea32308da5beaad20
Marin <mrbumbac@gmail.com>
2020-02-04 16:04:47 +0200

    edited

--- lab_1/main.js
+++ lab_1/main.js
@@ -1,69 +1,84 @@
 class Calculate {
-    constructor(array) {
-        this.array = array;
+  constructor(array) {
+    this.array = array;
+  }
+
+  maxMin() {
+    let max = this.array[0];
+    let min = this.array[0];
+
+    for (const item of this.array) {
+      if (item > max) {
+        max = item;
+      }
+      if (item < min) {
+        min = item;
+      }
+    }
+    return {min, max};
+  }
+
+  valueToCompare() {
+    const valueToCompare = document.getElementById("valueToCompare").value;
+    let equal = 0;
+    let smaller = 0;
+    let bigger = 0;
+
+      for (const item of this.array) {
+      if (item === valueToCompare) {
+        equal++;
+      }
+      if (item < valueToCompare) {
+        smaller++;
+      }
+      if (item > valueToCompare) {
+        bigger++;
+      }
     }
 
-    max() {
-        let max = this.array[0];
-        let len = this.array.length;
-
-        for (let i = 1; i < len; i++) {
-            if (this.array[i] > max) max = this.array[i];
-        }
-        return max;
+    return {
+      equal, smaller, bigger
     }
+  }
 
-    min() {
-        let min = this.array[0];
-        let len = this.array.length;
+  average() {
+    return this.array.reduce((sum, accumulator) => sum + accumulator) / this.array.length;
+  }
 
-        for (let i = 1; i < len; i++) {
-            if (this.array[i] < min) min = this.array[i];
-        }
-        return min;
-    }
-
-    valueToCompare() {
-        const value = document.getElementById("valueToCompare").value;
-        let equal = 0;
-        let smaller = 0;
-        let bigger = 0;
-
-        for (let i = 0; i <= this.array.length; i++) {
-            if (this.array[i] == value) equal++;
-            if (this.array[i] < value) smaller++;
-            if (this.array[i] > value) bigger++;
-        }
-
-        return {
-            equal, smaller, bigger
-        }
-    }
-
-    average() {
-        return this.array.reduce((sum, accumulator) => sum + accumulator) / this.array.length;
-    }
-
-    lowTemperatures() {
-        return this.array.reduce((count, x) => (x < 0 ? count + 1 : count), 0);
-    }
+  lowTemperatures() {
+    return this.array.filter(item => item < 0).length
+  }
 }
 
-const print = (label, number) => {
-    document.getElementById("results").innerHTML += `${label} :${number} <br>`;
+const init = () =>{
+  addListeners();
 };
 
-document.getElementById("calculate").addEventListener("click", () => {
+const resultElement = document.getElementById("results");
+
+const print = (label, number) => {
+  resultElement.innerHTML += `${label} :${number} <br>`;
+};
+
+const addListeners = () =>{
+  document.getElementById("calculate").addEventListener("click", () => {
     let temps = document.getElementById("temperatureHolder").value.split(" ").map(Number);
     const results = new Calculate(temps);
 
-    print("Lowest temperature:", results.min());
-    print("Highest temperature:", results.max());
-    print("Decalajul:", results.max() - results.min());
+    print("Lowest temperature:", results.maxMin().min);
+    print("Highest temperature:", results.maxMin().max);
+    print("Decalajul:", results.maxMin().max - results.maxMin().min);
     print("Average is:", results.average());
     print("Low temperatures are", results.lowTemperatures());
     print("Equal temperatures with indicated:", results.valueToCompare().equal);
     print("Smaller temperatures with indicated:", results.valueToCompare().smaller);
     print("Bigger temperatures with indicated:", results.valueToCompare().bigger);
-});
+  });
+};
+
+init();
+
+
+
+
 
